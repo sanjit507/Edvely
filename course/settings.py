061ab281c66a8dfe28535e77faf9a05487a1367d
Sanjit Chaudhary <sanjitchaudhary506@gmail.com>
2026-03-09 00:21:18 +0530

    Allow all hosts in settings

--- course/settings.py
+++ course/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-n@e5=m9_952+p+5%fmll_(q&vkcme^vei3q8aei7lgl(2uv7)t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
